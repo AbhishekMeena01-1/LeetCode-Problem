27aca62ec3d6d397c2bc2fedc8ad540d18f7770a
Abhishek Meena <meeaabhishek49@gmail.com>
2026-08-23 23:28:31 +0530

    Time: 4 ms (15.69%), Space: 45 MB (5.65%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -9,9 +9,13 @@ class Solution {
                 j++;
             } else {
 
-                if (s.charAt(i) != s.charAt(i + 1) &&
-                    s.charAt(i + 1) != s.charAt(i + 2) &&
-                    s.charAt(i) != s.charAt(i + 2)) {
+                HashSet<Character> set = new HashSet<>();
+
+                set.add(s.charAt(i));
+                set.add(s.charAt(i + 1));
+                set.add(s.charAt(i + 2));
+
+                if (set.size() == 3) {
                     count++;
                 }
 
